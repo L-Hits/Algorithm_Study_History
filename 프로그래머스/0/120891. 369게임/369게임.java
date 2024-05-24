1f5b357f9a3b6e76c41d9a4002c42da85cbb50b1
L-Hits <130430768+L-Hits@users.noreply.github.com>
2024-05-24 22:34:41 +0900

    [level 0] Title: 369게임, Time: 0.11 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int order) 
+    {
+        int answer = 0;
+        String next_order = Integer.toString(order);
+        
+        String[] num = next_order.split("");
+        
+        for(int i = 0; i<num.length; i++)
+        {
+            if(num[i].equals("3") || num[i].equals("6") || num[i].equals("9"))
+            {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
