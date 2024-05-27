fe0244ed936cb3679c071bcd1b465d5455257041
L-Hits <130430768+L-Hits@users.noreply.github.com>
2024-05-27 23:49:04 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+
+       
+        if(Math.sqrt(n) % 1 == 0)
+            return 1;
+        else
+            return 2;
+        
+        
+    }
+}
