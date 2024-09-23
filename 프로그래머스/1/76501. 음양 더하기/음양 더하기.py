f6b01956f2593c6ff102eee6cd48e992420ec146
L-Hits <130430768+L-Hits@users.noreply.github.com>
2024-09-23 13:32:57 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    
+    for i in range( len(absolutes) ):
+        if not signs[i]:
+            absolutes[i] *= -1
+            
+            
+    
+            
+    return sum(absolutes)
