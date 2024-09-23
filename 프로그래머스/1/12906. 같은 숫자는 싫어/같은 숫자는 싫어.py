a6694b32b18b7e49a92b60ce1368380d4269500f
L-Hits <130430768+L-Hits@users.noreply.github.com>
2024-09-24 00:38:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 77.08 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    answer = []
+    same_num = arr[0]
+    answer.append(arr[0])
+    
+    for i in range(len(arr)):
+        if arr[i] != same_num:
+            same_num = arr[i]
+            answer.append(arr[i])
+    
+    
+    
+    
+    return answer
