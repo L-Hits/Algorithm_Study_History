a9401249988f7962c02b559a7d5f04f49f9e7232
L-Hits <130430768+L-Hits@users.noreply.github.com>
2025-03-22 00:45:22 +0900

    [Bronze I] Title: 단어 공부, Time: 220 ms, Memory: 34368 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,16 @@
+str = input().upper()
+char_count = {}
+
+for char in str:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1
+
+max_count = max(char_count.values())
+most_frequent_chars = [char for char, count in char_count.items() if count == max_count]
+
+if len(most_frequent_chars) > 1:
+    print('?')
+else:
+    print(most_frequent_chars[0])
