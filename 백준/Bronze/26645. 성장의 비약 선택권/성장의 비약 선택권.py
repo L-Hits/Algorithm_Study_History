1f2704f4841f75e33eef3cf1794a91e2434db7f4
Hits <130430768+L-Hits@users.noreply.github.com>
2025-04-19 13:15:06 +0900

    [Bronze III] Title: 성장의 비약 선택권, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+
+if N >= 200 and N <= 205 : 
+    print(1)
+elif N >= 206 and N <= 217 : 
+    print(2)
+elif N >= 218 and N <= 228 : 
+    print(3)
+else : 
+    print(4)
